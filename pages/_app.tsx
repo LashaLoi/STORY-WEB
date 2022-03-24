a7b7f37b31017f19f04b97f52a5e8f90faf83dea
Aliaksei Loi <alex.loi@pandadoc.com>
2022-03-24 11:21:27 +0300

    finish app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { MantineProvider } from "@mantine/core";
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: "light"
+      }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
-
-export default MyApp
